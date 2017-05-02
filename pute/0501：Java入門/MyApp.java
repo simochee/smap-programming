b9dea494979d5d125047c5c79d99c6ff82558211
simochee <simochee@gmail.com>
2017-05-02 20:53:46 +0900

    Save

---
+++ pute/0501：Java入門/MyApp.java
@@ -0,0 +1,16 @@
+public class MyApp
+{
+public static void main( String[] args)
+	{
+		int number;
+		number=100;
+		
+		int number2=3;
+		
+           System.out.println(number/number2);
+    }
+}
+
+
+
+
