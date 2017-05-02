7012191e5b9d738fb33fc96614cd1f1f52fcdd10
simochee <simochee@gmail.com>
2017-05-02 20:43:23 +0900

    Save

---
+++ miporin/0502：Java入門/MyApp.java
@@ -0,0 +1,16 @@
+public class MyApp
+{
+	public static void main( String[] args )
+	{
+		int number;
+		number = 100;
+		
+		int number2 = 3;
+		
+		System.out.println(number / number2);
+	}
+}
+
+
+
+„‚Ì–¼‘O‚Íd­”ü•à‚Å‚·
